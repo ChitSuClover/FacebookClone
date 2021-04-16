f3631dff1f615bfed24184b2398105517452dcff
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-16 22:02:11 +0630

    User Email バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :name ,:presence: true, length: {maximum: 20}
+  validates :email ,:presence: true, length: {maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
 end

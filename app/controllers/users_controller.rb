55a565226eb35b68b75bf215a2cd417b33353147
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-17 17:35:31 +0630

    feat: User show アクションを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user=User.new
   end
   def create
-    @user = User.new(user_params)
+    @user=User.new(user_params)
     if @user.save
-      redirect_to :user_path
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+  def show
+    @user=User.find(params[:id])
   end
   private
   def user_params

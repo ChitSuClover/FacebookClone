609f93016fcf7208f05b95deed7e2050e9fcf46a
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-18 10:36:47 +0630

    feat:New & Create アクションを設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,8 +3,21 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+  def new
+    @blog = Blog.new
+  end
   def create
-    @blog = Blog.new()
+    if params[:back]
+      @blog = Blog.new(blog_params)
+      render :new
+    else
+      @blog = Blog.create(blog_params)
+      if @blog.save
+        redirect_to blogs_path, notice: "Post Created"
+      else
+        render :new
+      end
+    end
   end
   private
   def set_blog

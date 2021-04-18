794d1f1a179e47fc6743ebba57c1d18dfaba7ab2
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-18 10:26:05 +0630

    feat:Controller private メソードを追記

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,16 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only:[:show, :edit, :update, :destroy]
   def index
-    @blogs =Blog.all
+    @blogs = Blog.all
   end
-  
+  def create
+    @blog = Blog.new()
+  end
+  private
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+  def blog_params
+    params.require(:blog).permit(:content, :image, :image_cache)
+  end
 end
